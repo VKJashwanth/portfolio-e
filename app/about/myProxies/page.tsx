16939dea2f5c99c00d9ec11dbd1e13130828a2ef
Jashwanth <vk.vkjashwanth@gmail.com>
2023-10-13 20:56:41 +0000

    build folder for deployment

--- app/about/myProxies/page.tsx
+++ app/about/myProxies/page.tsx
@@ -1,7 +1,16 @@
+"use client"
+import { useState } from "react"
 export default function MyProxies() {
+    const [dark, setDarkMode] = useState(true)
     return (
-        <>
-            
-        </>
+        <div className={dark ? "dark" : ""}>
+            <main className="bg-white px-10 overflow-x-hidden dark:bg-gray-900 md:px-20 lg:px-40 ">
+                <section>
+                    <div className="dark:text-white">
+                        <div className="text-center shadow-lg pt-8 pb-2"></div>
+                    </div>
+                </section>
+            </main>
+        </div>
     )
 }
